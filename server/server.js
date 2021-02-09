aa5b939d51035a3229a62a19ef3752aae02f9995
akshay1027 <akshayar1027@gmail.com>
2021-02-09 23:38:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("server");
+const express = require("express");
+
+const app = express();
+
+app.post('/memes', (req, res) => {
+    console.log(req.body)
+    console.log("hi")
+})
+
+app.listen(8080, function () {
+    console.log('listening on 8080');
+})
