a9fe95923a4b09d501df0f0947a78e91cf0340d3
akshay1027 <akshayar1027@gmail.com>
2021-02-11 04:27:26 +0530

    Update card.jsx

--- client/src/components/mainpage/card.jsx
+++ client/src/components/mainpage/card.jsx
@@ -6,11 +6,13 @@ import axios from "../../helper/axiosHelper";
 function MainPage(props) {
 
     return (
-        <div className="card rounded lg=10" >
+        <div className="card rounded border-warning rounded p-10 xl=6 lg=6" 
+        style={{width: "20rem", margin:"20px" }}>
             <div className="card-body">
                     <div>
-                        <strong>{props.Name}</strong>
-                        <strong>{props.caption}</strong>
+                        <h5><strong>Name: {props.Name}</strong></h5>
+                        <br />
+                        <h5><strong>Caption: {props.caption}</strong></h5>
                     </div>
             </div>
             <img src={props.url} alt={props.caption} className="card-img-top" />
