906fa9df33b37fecde8789885d741ee91f7e51b6
akshay1027 <akshayar1027@gmail.com>
2021-02-12 15:18:32 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import MemeForm from "./components/memeForm/memeForm";
-import GetMemes from "./components/getMemes/list"
-import Main from "./components/mainpage/card"
+import GetMemes from "./components/getMemes/list";
+import Header from "./components/header/header";
  
 function App() {
   return (
     <div>
+      <Header />
       <MemeForm />
       <GetMemes />
     </div>
