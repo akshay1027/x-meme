7e422523a300becb296383170422ebed84ce21fc
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-02-14 22:38:51 +0530

    Add files via upload

--- client/src/components/mainpage/card.jsx
+++ client/src/components/mainpage/card.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import axios from "../../helper/axiosHelper";
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -12,11 +12,11 @@ function MainPage(props) {
         <Col xs={12} md={10} lg={10}></Col>
         <div className="card rounded rounded" 
         style={{width: "20rem", margin:"20px", padding:"12px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-    }}>
+        }}>
             <div className="card-body">
-                    <div>
-                        <h6>{props.Name}</h6>
-                        <h5><strong>{props.caption}</strong></h5>
+                    <div style={{marginLeft:"-10px"}}>
+                        <h5>{props.Name}</h5>
+                        <h4><strong>{props.caption}</strong></h4>
                     </div>
             </div>
             <img src={props.url} alt={props.caption} className="card-img-top" loading="lazy"/>
@@ -28,4 +28,4 @@ function MainPage(props) {
 
 export default MainPage;
 
- 
+  
