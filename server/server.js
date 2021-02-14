6a265d182cdc9d1c86b523668a118c7b31f57535
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-02-14 21:46:32 +0530

    change in server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const logger = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const FileSync = require("lowdb/adapters/FileSync");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
 // low db
 const adapter = new FileSync("db.json");
@@ -31,23 +31,21 @@ db.defaults(defaultData).write();
 const app = express();
 app.use(bodyParser.json());
 
-app.use(
-  cors()
-);
+app.use(cors());
 
 app.use(logger("dev"));
 
 //express
 
 app.get("/", function (req, res) {
-    res.send("hello from server");
-  });
+  res.send("hello from server");
+});
 
 app.post("/memes", (req, res) => {
   const name = req.body.name;
   const caption = req.body.caption;
   const url = req.body.url;
-  
+
   const lastMeme = db.get("memes").takeRight(1).value()[0];
   console.log(lastMeme);
   const nextId = lastMeme.id + 1;
@@ -66,11 +64,12 @@ app.post("/memes", (req, res) => {
 // getting latest 100 memes
 
 app.get("/memes", function (req, res) {
-  const data = db.get("memes").slice(db.length - 101).value();
-  res.send(data);
+  let data = db.get("memes").value();
+  let cloneData = [...data].reverse().slice(0, 100);
+  res.send(cloneData);
 });
 
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8081;
 
 app.listen(port, function () {
   console.log(`listening on ${port}`);
