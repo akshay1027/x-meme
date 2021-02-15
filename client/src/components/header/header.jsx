a2de010101d9f34797844c05df92dfbae5164302
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-02-15 05:58:38 +0530

    Update header.jsx

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,13 +1,18 @@
-import axios from "../../helper/axiosHelper";
 import { Navbar } from 'react-bootstrap';
 
 const Header = () => {
 
     return (
-
-            <Navbar bg="primary">
-            <Navbar.Brand style={{color: "white", fontSize:"30px"}}>XMEME</Navbar.Brand>
+            <>
+            <Navbar bg="primary" className="justify-content-between">
+            <Navbar.Brand style={{color: "white", fontSize:"35px", fontFamily:"monospace"  }}>XMEME</Navbar.Brand>
+            <Navbar.Brand href="https://akshayrr-xmeme.herokuapp.com/swagger-ui/" target="_blank" 
+                style=
+                {{color: "white", fontSize:"15px", fontFamily:"monospace", marginBottom:"-2px", padding:"6px", border:"2px solid white" 
+                }}>Swagger-UI
+            </Navbar.Brand>
             </Navbar>
+            </>
         );
 }
 
